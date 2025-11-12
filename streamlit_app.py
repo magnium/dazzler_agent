db7ceda4a878e02f8fba376d66729709afdbb52b
magnium <magnium@users.noreply.github.com>
2025-11-12 17:07:32 +0000

    updated to mistral conversation API

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,15 @@ import streamlit as st
 from mistralai import Mistral
 
 client = Mistral(api_key="DIiJ24tz6bS9ddYHBjRcCMc2kFwV17cs")
-model = "mistral-tiny"
 agent = "ag_019a789fcbc37673b376c974ba2cc120"#"ag_019a783390427144bca557571b4c11e8"
 st.title("Dazzler AI Agent")
 user_input = st.text_input("Ask me anything about Dazzler:")
 if user_input:
-    response = client.agents.complete(
+    response = client.beta.conversations.start(
         agent_id=agent,
-        messages=[{"role": "user", "content": user_input}]
+        inputs=user_input
     )
-    st.write(response.choices[0].message.content)
+    if response.outputs:
+        st.write(response.outputs[0].content)
+    else:
+        st.write("No response from the agent.")
