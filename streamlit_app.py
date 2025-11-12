a5ad6091616594a37e7d56b245deeb4e73da3f20
magnium <magnium@users.noreply.github.com>
2025-11-12 15:14:52 +0000

    update to new agent

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from mistralai import Mistral
 
 client = Mistral(api_key="DIiJ24tz6bS9ddYHBjRcCMc2kFwV17cs")
 model = "mistral-tiny"
-agent = "ag_019a783390427144bca557571b4c11e8"
+agent = "ag_019a789fcbc37673b376c974ba2cc120"#"ag_019a783390427144bca557571b4c11e8"
 st.title("Dazzler AI Agent")
 user_input = st.text_input("Ask me anything about Dazzler:")
 if user_input:
--- test.py
+++ test.py
@@ -1,6 +1,16 @@
 from mistralai import Mistral
 api_key = "DIiJ24tz6bS9ddYHBjRcCMc2kFwV17cs"
 client = Mistral(api_key=api_key)
-libraries = client.beta.libraries.list().data
-for library in libraries:
-    print(library.name, f"with {library.nb_documents} documents.")
+library_agent = client.beta.agents.create(
+    model="mistral-medium",
+    name="Dazzler AI agent",
+    description="Agent used to access documents from the Dazzler library.",
+    instructions="Answer questions about properties and usage of Dazzler (AOPDF, Acousto-optic programmable dispersive filter). Exclude all results related to Comic books or weapons. Use https://amplitude-laser.com/products/femtosecond-lasers/instrumentation-lasers-femtosecondes/dazzler/ for additional information.",
+    tools=[{"type": "document_library", "library_ids": 
+            ["019a77b0-594c-740d-ac5d-d5d8531479c1"]}],
+    completion_args={
+        "temperature": 0.3,
+        "top_p": 0.95,
+    }
+)
+print(library_agent)
